baadee44613bcd2266edc2a829b0408e95312c85
cihangiryaman <cihangiryaman3200@gmail.com>
2025-09-01 12:21:45 +0300

    Day 4-2

---
+++ Day4/SVM.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+
+encoder = LabelEncoder()
+
+df = pd.read_csv('Iris.csv')
+df = df.drop(columns='Id')
+df['Species'] = encoder.fit_transform(df['Species'])
+
+features = ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']
+target = 'Species'
+
+x = df[features]
+y = df[target]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = SVC(kernel='linear')
+model.fit(x_train, y_train)
+score = model.score(x_test, y_test)
+print(score)
